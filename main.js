6c2a160e2cdcc2ba8db23d9e374ab134dbb37866
Oliver <oliver.hilferty@rezonence.com>
2019-04-05 12:48:40 +0100

    Add basic downloading logic

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ let ArgumentParser = require('argparse').ArgumentParser;
 let request = require('request');
 let cheerio = require('cheerio');
 let chalk = require('chalk');
+let path = require('path');
 let url = require('url');
 let fs = require('fs');
 
@@ -23,5 +24,30 @@ parser.addArgument(
 );
 
 let args = parser.parseArgs();
+
+request({
+    uri: args.url,
+}, function(error, response, body) {
+    let $ = cheerio.load(body);
+
+    let imgs = $('img');
+
+    imgs.each(function() {
+        let img = $(this);
+        let imgSrc = img.attr('src');
+        let imgPath = url.resolve(args.url, imgSrc);
+        let splitPath = imgPath.split('/');
+        let filePath = path.join((args.destination || './'), splitPath[splitPath.length - 1]);
+
+        console.log(imgPath, filePath);
+
+        download(imgPath, filePath);
+    });
+});
+
+
+let download = (uri, filePath) => {
+    request(uri).pipe(fs.createWriteStream(filePath));
+};
 
 console.log(args);
